0243e2aa9bd54e4e7e43a41c7cbf2b7f0a6c68fd
KvotheGuile <kvothejoestar@gmail.com>
2026-04-03 23:43:59 -0700

    fix: removed unused function

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -15,26 +15,6 @@ import { getAllProducts } from "../api/productapi";
 
 interface Props {}
 
-const SortIcon = ({ className }: { className?: string }) => (
-  <svg
-    className={className}
-    aria-hidden="true"
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    fill="none"
-    viewBox="0 0 24 24"
-  >
-    <path
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="2"
-      d="m8 15 4 4 4-4m0-6-4-4-4 4"
-    />
-  </svg>
-);
-
 
 const ProductPage: React.FC<Props> = () => {
 
